2dcc9d9c63a983ded5363f334f75c496bfccdeaf
Josenilson Farias <fariaslwork@gmail.com>
2023-08-17 15:56:06 -0300

    refactor: dashboard, props and classnames

--- src/pages/Dashborad/Dashborad.jsx
+++ src/pages/Dashborad/Dashborad.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
 import style from "./style.module.scss";
+import { useUserContext } from "../../providers/UserContext";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 
-export const Dashboard = ({ user, setUser }) => {
-
+export const Dashboard = () => {
+    const navi = useNavigate();
+    const { user } = useUserContext();
     return (
         <div className={style.div}>
-            <Header visible={true} user={user} setUser={setUser}/>
+            <Header visible={true} />
             <main className={style.main}>
-                <div className={style.user}>
+                <div className={style.user_}>
                     <div className="container">
                         <h2 className="title white">Olá {user.name}</h2>
                         <span className="title grey headline">
